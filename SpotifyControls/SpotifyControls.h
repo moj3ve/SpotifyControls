a008cc5851481875800d0b422500959def56c0a9
MinhTon <ford.tonthat@gmail.com>
2020-07-19 15:00:06 +0700

    Initial Commit

---
+++ SpotifyControls/SpotifyControls.h
@@ -0,0 +1,19 @@
+//
+//  SpotifyControls.h
+//  SpotifyControls
+//
+//  Created by Ford on 7/19/20.
+//  Copyright © 2020 MinhTon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpotifyControls.
+FOUNDATION_EXPORT double SpotifyControlsVersionNumber;
+
+//! Project version string for SpotifyControls.
+FOUNDATION_EXPORT const unsigned char SpotifyControlsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpotifyControls/PublicHeader.h>
+
+
